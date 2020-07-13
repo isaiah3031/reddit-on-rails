e91a1eb119117705f4d1dd27ac15797cab36d7be
isaiah3031 <isaiah.whitfield@gmail.com>
2020-07-13 10:05:17 -0400

    Added feature test 'Users must log in before creating a sub'

--- spec/features/sub_spec.rb
+++ spec/features/sub_spec.rb
@@ -8,10 +8,20 @@ feature 'Displays subs' do
 end
 
 feature 'Creating subs' do
-  scenario 'allows users to create subs' do
+  scenario 'Allows users to create subs' do
     create_user(Time.now.to_s)
     visit subs_url
     expect(page).to have_link('', href: new_sub_url) 
     expect(page).to have_text('Create Sub')
+  end
+
+  scenario 'Users must log in before creating a sub' do
+    create_user(Time.now.to_s)
+    visit subs_url
+    expect(page).to have_link('', href: new_sub_url) 
+    expect(page).to have_text('Create Sub')
+    logout
+    expect(page).not_to have_link('', href: new_sub_url)
+    expect(page).not_to have_text('Create Sub')
   end
 end
